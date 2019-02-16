785c5fdd1be05f7c1a17b9293afab43575c0f224
monaru1124 <monaru1124@naver.com>
2019-02-16 10:47:46 +0900

    이벤트 처리 작업중
    - 버튼이 눌리면 할 일을 인터페이스를 통해서 재하청 맡김.

--- src/code/Button.java
+++ src/code/Button.java
@@ -4,10 +4,22 @@ public class Button {
 
 //	TODO - 눌린다! 기능만. (메쏘드)
 	
+	ButtonClickListener bcl;
+	
 	public void click() {
 		
-		
+//		클릭 메쏘드 처리를 버튼클릭리스너 역할을 수행하는 클래스에게 넘긴다!
+		bcl.onClick();
 
 	}
+	
+	public void setButtonClickListner(ButtonClickListener inputBcl) {
+		
+//		누군가 처리 대행을 자원하면, 그 역할을 받아들임.
+		
+		this.bcl = inputBcl;
+				
+	}
+	
 	
 }
--- src/code/LoginActivity.java
+++ src/code/LoginActivity.java
@@ -1,24 +1,41 @@
 package code;
 
-public class LoginActivity {
+public class LoginActivity implements ButtonClickListener {
+	
+	static Button button1 ;
+	static EditText idEdt;
 	public static void main(String[] args) {
 		
 //		버튼이 눌리면 => 에딧텍스트에 입력된 값을 출력.
 		
-		Button button1 = new Button();
 		
-		button1.click();
+		button1 = new Button();
+
 		
 //		ID를 입력받는 EditText 객체화
 		
-		EditText idEdt = new EditText();
+
 		
 //		idEdt에 monaru1124라는 값을 입력.
 		
+		idEdt = new EditText();
 		idEdt.setText("monaru1124");
+		
+//		실제로 버튼을 클릭!
+		button1.click();
 		
 		
 		
 		
 	} // main
+
+//	버튼이 클릭되면 실행될 내용을 실질적으로 작성.
+	@Override
+	public void onClick() {
+		// TODO Auto-generated method stub
+		
+		System.out.println(idEdt.getText());
+		
+		
+	} // on click
 }
