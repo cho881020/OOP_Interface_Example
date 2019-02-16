b980cb5e4bfe4515b9b9194d63cbcc39538b67cf
shairyu93 <shanav@naver.com>
2019-02-16 10:53:45 +0900

    매인메소드 내용을 클래스로 옮김
    매인드라이브 내용을 로그인클래스로 내용을 옮김.

--- src/code/LoginActivity.java
+++ src/code/LoginActivity.java
@@ -1,31 +1,12 @@
 package code;
 
-public class LoginActivity implements ButtonClickListener{
-
-	static Button button1;
-	static EditText idEdt;
+public class LoginActivity {
 	
-	public static void main(String[] args) {
-//		버튼이 눌리면 => 에딧텍스트에 입력된 값을 출력
+	Button button1;
+	EditText idEdt;
+	
+	public void runSceinario() {
 		
-		button1 = new Button();
-		
-		button1.click();
-		
-		idEdt = new EditText();
-		
-//		idEdt에 shairyu이라는 값을 입력.
-		idEdt.setText("shairyu");
-		
-//		실제로 버튼을 클릭한다
-		button1.click();
 	}
 
-//	버튼이 클릭되면 실행될 내용을 실질적으로 작성.
-	@Override
-	public void onClick() {
-		
-		System.out.println(idEdt.getText());
-		
-	}
 }
