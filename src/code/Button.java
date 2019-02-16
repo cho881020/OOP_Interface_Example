bd596af8e9c122108d36f299107c02ce37e162b7
Psojeong <6muse.on@gmail.com>
2019-02-16 10:32:40 +0900

    시나리오 설정
    - 버튼 클래스 작성 (클릭기능만)
    - EditText 클래스 작성 (입력값 속성)
    - 시나리오 : 버튼이 눌리면 입력된 id를 콘솔에 출력하고자 함.

---
+++ src/code/Button.java
@@ -0,0 +1,11 @@
+package code;
+
+public class Button {
+	
+//	눌린다! 기능만 (메쏘드)
+	
+	public void click() {
+		
+	}
+
+}
---
+++ src/code/EditText.java
@@ -0,0 +1,17 @@
+package code;
+
+public class EditText {
+	
+	private String test;
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+	
+	
+
+}
