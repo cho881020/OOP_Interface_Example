67c91f42cc818044e24515c59a30557d04714e06
monaru1124 <monaru1124@naver.com>
2019-02-16 10:53:23 +0900

    코드 구조 변경
    메인드라이브 작성내용 => 로그인액티비티
    메인드라이브는 단순히 로그인액티비티 실행만

--- src/code/LoginActivity.java
+++ src/code/LoginActivity.java
@@ -1,41 +1,5 @@
 package code;
 
-public class LoginActivity implements ButtonClickListener {
-	
-	static Button button1 ;
-	static EditText idEdt;
-	public static void main(String[] args) {
-		
-//		버튼이 눌리면 => 에딧텍스트에 입력된 값을 출력.
-		
-		
-		button1 = new Button();
+public class LoginActivity {
 
-		
-//		ID를 입력받는 EditText 객체화
-		
-
-		
-//		idEdt에 monaru1124라는 값을 입력.
-		
-		idEdt = new EditText();
-		idEdt.setText("monaru1124");
-		
-//		실제로 버튼을 클릭!
-		button1.click();
-		
-		
-		
-		
-	} // main
-
-//	버튼이 클릭되면 실행될 내용을 실질적으로 작성.
-	@Override
-	public void onClick() {
-		// TODO Auto-generated method stub
-		
-		System.out.println(idEdt.getText());
-		
-		
-	} // on click
 }
