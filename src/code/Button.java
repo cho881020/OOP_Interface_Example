82e554b5ca86a1d50b813d0c8503f0cdeae92f82
shairyu93 <shanav@naver.com>
2019-02-16 10:32:37 +0900

    시나리오 설정
    버튼생성
    클래스 만듦.
    EditText 생성
    클래스를 만듦.
    입력값을 속성으로 가지고 있음
    버튼이 눌리면 입력된 아이디를 Console에 출력하도록 만들라고함

---
+++ src/code/Button.java
@@ -0,0 +1,9 @@
+package code;
+
+public class Button {
+
+//	 눌린다! 라는 기능만 있으면 됨!
+	public void click() {
+		
+	}
+}
---
+++ src/code/EditText.java
@@ -0,0 +1,15 @@
+package code;
+
+public class EditText {
+
+	String Text;
+
+	public String getText() {
+		return Text;
+	}
+
+	public void setText(String text) {
+		Text = text;
+	}
+	
+}
