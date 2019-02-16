f04906e4bbfc676c89fc94421fb61377fea8cf21
si15 <si09@naver.com>
2019-02-16 11:05:11 +0900

    코드 수정

--- src/code/EditText.java
+++ src/code/EditText.java
@@ -1,5 +1,17 @@
 package code;
 
 public class EditText {
+	
+	private String text;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
 
 }
