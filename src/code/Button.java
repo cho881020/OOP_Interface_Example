b604ce52f54718cb2893d9540cc7c756498018da
si15 <si09@naver.com>
2019-02-16 10:33:41 +0900

    시나리오 설정
    - 버튼 클래스 작성(클릭기능만)
    - 에디텟스트 클래스 작성(입력값 속성)
    - 버튼이 눌리면 입력된 id를 콘솔에 출력

---
+++ src/code/Button.java
@@ -0,0 +1,9 @@
+package code;
+
+public class Button {
+	
+//	todo - 눌린다! 기능만.
+	
+	
+
+}
---
+++ src/code/LoginActivity.java
@@ -0,0 +1,16 @@
+package code;
+
+public class LoginActivity {
+	
+	public static void main(String[] args) {
+		
+		
+//	 버튼이 눌리면 => 에디텍스트에 입력된 값을 출력
+		
+		Button button1 = new Button();
+		
+		button1.click();
+		
+	}
+
+}
