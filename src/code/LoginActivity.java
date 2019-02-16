237ee97f332aef04de74b3885070ae3926b66398
SeulkiOh <ohseulki23@naver.com>
2019-02-16 10:35:09 +0900

    id 입력용 에딧텍스트 객체화

--- src/code/LoginActivity.java
+++ src/code/LoginActivity.java
@@ -10,6 +10,12 @@ public class LoginActivity {
 		
 		button1.click();
 		
+//		id를 입력받는 EditText 객체화
+		EditText idEdt = new EditText();
+		
+//		idEdt에 ohseulki23이라는 값을 입력.
+		idEdt.setId("ohseulki23");
+		
 	}
 
 }
