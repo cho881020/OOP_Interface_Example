8ad26a196c60d7a958217293e4b336cbe08070f7
SeulkiOh <ohseulki23@naver.com>
2019-02-16 10:47:25 +0900

    이벤트 처리 작업중
    - 버튼이 눌리면 할 일을 인터페이스를 통해서 재하청 맡김.

--- src/code/LoginActivity.java
+++ src/code/LoginActivity.java
@@ -1,20 +1,33 @@
 package code;
 
-public class LoginActivity {
-	
+public class LoginActivity implements ButtonClickListener{
+	static Button button1;
+	static EditText idEdt;
 	public static void main(String[] args) {
 		
 //		버튼이 눌리면 에딧텍스트에 입력된 값을 출력.
 		
-		Button button1 = new Button();
+		button1 = new Button();
 		
 		button1.click();
 		
 //		id를 입력받는 EditText 객체화
-		EditText idEdt = new EditText();
+		idEdt = new EditText();
 		
 //		idEdt에 ohseulki23이라는 값을 입력.
 		idEdt.setId("ohseulki23");
+		
+//		실제로 버튼을 클릭!
+		button1.click();
+		
+	}
+
+//	버튼이 클릭되면 실행될 내용을 실질적으로 작성.
+	@Override
+	public void onClick() {
+		// TODO Auto-generated method stub
+		
+		System.out.println(idEdt.getId());
 		
 	}
 
