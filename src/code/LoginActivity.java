912cd39d7cd626eb77676f7e4770e69004a17156
monaru1124 <monaru1124@naver.com>
2019-02-16 10:35:13 +0900

    id 입력용 에딧텍스트 객체화

--- src/code/LoginActivity.java
+++ src/code/LoginActivity.java
@@ -9,6 +9,15 @@ public class LoginActivity {
 		
 		button1.click();
 		
+//		ID를 입력받는 EditText 객체화
+		
+		EditText idEdt = new EditText();
+		
+//		idEdt에 monaru1124라는 값을 입력.
+		
+		idEdt.setText("monaru1124");
+		
+		
 		
 		
 	} // main
