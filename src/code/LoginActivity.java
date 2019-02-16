2a8e3e688e027ac824bc62621786d19dcb519674
monaru1124 <monaru1124@naver.com>
2019-02-16 10:53:58 +0900

    코드구조변경(re)
    메인드라이브 작성내용 => 로그인액티비티
    메인드라이브는 단순히 로그인액티비티 실행만

--- src/code/LoginActivity.java
+++ src/code/LoginActivity.java
@@ -1,5 +1,37 @@
 package code;
 
-public class LoginActivity {
+public class LoginActivity implements ButtonClickListener {
+	Button button1 ;
+	EditText idEdt;
+	public void runSceinario() {
+		
+//		버튼이 눌리면 => 에딧텍스트에 입력된 값을 출력.
+		
+		button1 = new Button();
+
+		
+//		ID를 입력받는 EditText 객체화		
+		idEdt = new EditText();
+
+		
+//		idEdt에 monaru1124라는 값을 입력.		
+		idEdt.setText("monaru1124");
+		
+		button1.setButtonClickListner(this);
+		
+//		실제로 버튼을 클릭!
+		button1.click();
+		
+	}
+	
+//	버튼이 클릭되면 실행될 내용을 실질적으로 작성.
+	@Override
+	public void onClick() {
+		// TODO Auto-generated method stub
+		
+		System.out.println(idEdt.getText());
+		
+		
+	} // on click
 
 }
--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -1,40 +1,15 @@
 package code;
 
-public class MainDrive implements ButtonClickListener {
+public class MainDrive {
 	
-	static Button button1 ;
-	static EditText idEdt;
+
 	public static void main(String[] args) {
 		
-//		버튼이 눌리면 => 에딧텍스트에 입력된 값을 출력.
+		LoginActivity la1 = new LoginActivity();
 		
-		button1 = new Button();
-
-		
-//		ID를 입력받는 EditText 객체화		
-		idEdt = new EditText();
-
-		
-//		idEdt에 monaru1124라는 값을 입력.		
-		idEdt.setText("monaru1124");
-		
-		button1.setButtonClickListner(this);
-		
-//		실제로 버튼을 클릭!
-		button1.click();
-		
-		
-		
+		la1.runSceinario();
 		
 	} // main
 
-//	버튼이 클릭되면 실행될 내용을 실질적으로 작성.
-	@Override
-	public void onClick() {
-		// TODO Auto-generated method stub
-		
-		System.out.println(idEdt.getText());
-		
-		
-	} // on click
+
 }
