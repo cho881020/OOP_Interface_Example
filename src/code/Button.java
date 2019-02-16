f995de356a79a77bf10435281c1649cf68ba64b3
shairyu93 <shanav@naver.com>
2019-02-16 10:47:37 +0900

    버튼 클릭 이벤트 작업
    버튼이 눌리면 처리하는 이벤트를 인터페이스 다시 재하청

--- src/code/Button.java
+++ src/code/Button.java
@@ -3,7 +3,18 @@ package code;
 public class Button {
 
 //	 눌린다! 라는 기능만 있으면 됨!
+	ButtonClickListener bcl;
+	
 	public void click() {
 		
+//		클릭 메소드 처리를 버튼클릭리스너 역할을 수행하는 클래스에게 넘긴다!.
+		bcl.onClick();
+	}
+	
+	public void setButtonClickListener(ButtonClickListener inputBcl) {
+		
+//		누군가가 처리대행을 자원하면 그 역활을 받아드리는 역할.
+		
+		this.bcl = inputBcl;
 	}
 }
