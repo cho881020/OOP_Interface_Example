0bc432eded0c6e3bcdee034b827bebb1d7ee829d
si15 <si09@naver.com>
2019-02-16 11:01:00 +0900

    메인코드 변경

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -1,39 +1,18 @@
 package code;
 
-public class MainDrive implements ButtonClickListner {
-	
-	static Button button1;
-	static EditText idEdt
+public class MainDrive {
+
 	
 	public static void main(String[] args) {
 		
+		LoginActivity la1 = new LoginActivity();
 		
-//	 버튼이 눌리면 => 에디텍스트에 입력된 값을 출력
-		
-		button1 = new Button();
-					
-		
-		
-		idEdt = new EditText();
-		
-		idEdt.setText("ksi");
-		
-		button1.setButtonClickListner(this);
-
-//		실제로 버튼을 클릭
-		button1.click();
-
+		la1.runSceinario();
 		
 	}
-
-	
-//	버튼이 클릭되면 실행될 내용을 실질적으로 작성.
-	@Override
-	public void onClick() {
-	// TODO Auto-generated method stub
-		System.out.println(idEdt.getText());
-		
-		
-	}
-
 }
+
+
+
+
+
